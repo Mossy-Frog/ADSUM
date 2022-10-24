5faa3f287f93d6dacb9ff274628a67ff2271815c
Mossy-Frog <86064030+Mossy-Frog@users.noreply.github.com>
2022-10-24 15:42:59 +0200

    Update list
    Change in the to update a list

--- mainstreamlit.py
+++ mainstreamlit.py
@@ -3,11 +3,13 @@ import pandas as pd
 import plotly.express as px
 
 
-st.text_input('Email address')
-st.radio('Pick your gender',['Male','Female'])
-st.text_area('Input symptoms of the patient')
-st.slider('Put a weight on the gravity of symptoms of the patient', 0,10)
+email = st.text_input('Email address')
+gender = st.radio('Pick your gender',['Male','Female'])
+symptoms = st.text_area('Input symptoms of the patient')
+weight = st.slider('Put a weight on the gravity of symptoms of the patient', 0,10)
 
-
+list_patient = []
+list_patient += [email,gender,symptoms,weight]
+print(list_patient)
 
 #streamlit run mainstreamlit.py
